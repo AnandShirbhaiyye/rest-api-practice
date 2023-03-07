9a42aa58a28e6b6ac4944807847052e33a8c9db1
AnandShirbhaiyye <anandshirbhaiyye@gmail.com>
2023-03-07 22:03:43 +0530

    database added

--- server/index.js
+++ server/index.js
@@ -1,8 +1,20 @@
 import express from 'express';
 const app = express();
 app.use(express.json());
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
 
 const PORT = process.env.PORT || 5000;
+
+
+try {
+  await mongoose.connect(process.env.MONGODB_URL, { useNewUrlParser: true });
+  console.log("Connected to MongoDB");
+} catch (error) {
+  console.error(error);
+}
+
 
 
 
